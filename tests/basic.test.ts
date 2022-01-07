2bc3bac5702902a2f71d87e2fbabd1bcacae319c
Alexandros Katechis <alexandros.katechis@clarifai.com>
2022-01-07 14:57:35 -0500

    build: package now comes with esm and commonjs formats for various tools

--- tests/basic.test.ts
+++ tests/basic.test.ts
@@ -1,14 +1,40 @@
-import { ClarifaiStub } from '../index'
+import { ClarifaiStub as StubSRC } from '../index'
+import { ClarifaiStub as StubCJS } from '../dist/cjs/index'
+import { ClarifaiStub as StubESM } from '../dist/esm/index'
 import { expect } from 'chai'
 
-describe('Basic Client Stub', () => {
+describe('Client Stub (src)', () => {
   it('works', () => {
-    const client = ClarifaiStub.grpc()
+    const client = StubSRC.grpc()
     expect(client.hostname_).to.equal('api.clarifai.com')
   })
 
   it('allows custom hostname', () => {
-    const client = ClarifaiStub.grpc('api-dev.clarifai.com')
+    const client = StubSRC.grpc('api-dev.clarifai.com')
+    expect(client.hostname_).to.equal('api-dev.clarifai.com')
+  })
+})
+
+describe('Client Stub (cjs)', () => {
+  it('works', () => {
+    const client = StubCJS.grpc()
+    expect(client.hostname_).to.equal('api.clarifai.com')
+  })
+
+  it('allows custom hostname', () => {
+    const client = StubCJS.grpc('api-dev.clarifai.com')
+    expect(client.hostname_).to.equal('api-dev.clarifai.com')
+  })
+})
+
+describe('Client Stub (esm)', () => {
+  it('works', () => {
+    const client = StubESM.grpc()
+    expect(client.hostname_).to.equal('api.clarifai.com')
+  })
+
+  it('allows custom hostname', () => {
+    const client = StubESM.grpc('api-dev.clarifai.com')
     expect(client.hostname_).to.equal('api-dev.clarifai.com')
   })
 })
