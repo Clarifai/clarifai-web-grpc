100e142a34254933439f08224557d6c23162effb
Alexandros Katechis <alexandros.katechis@clarifai.com>
2022-02-04 18:03:03 -0500

    fix(package)!: cleanse the package
    BREAKING CHANGE: Consumers must now import the fully qualified
    package names.

--- index.ts
+++
@@ -1,9 +0,0 @@
-import { V2Client } from './proto/clarifai/api/serviceServiceClientPb'
-
-export class ClarifaiStub {
-  static grpc (hostname = 'api-grpc-web.clarifai.com') {
-    return new V2Client(hostname)
-  }
-}
-
-export { V2Client }
