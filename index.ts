7030339944e334d24a5189d95363228531dac685
Alexandros Katechis <alexandros.katechis@clarifai.com>
2022-03-08 07:37:10 -0500

    fix: broken package
    BREAKING CHANGE:
    Completely reverts several versions of the package that were broken/attempting
    to provide a lean package. This is hopefully a known good state that fixes
    those issues.

---
+++ index.ts
@@ -0,0 +1,11 @@
+import { V2Client } from './proto/clarifai/api/serviceServiceClientPb'
+
+export class ClarifaiStub {
+  static grpc (hostname = 'api.clarifai.com') {
+    return new V2Client(hostname)
+  }
+}
+
+export { V2Client }
+export { Error, Metadata, ClientReadableStream } from 'grpc-web'
+export { BaseResponse } from './proto/clarifai/api/status/status_pb'
