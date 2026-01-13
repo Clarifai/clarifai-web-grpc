f183817623ed1bec855f458b9aa1674a2dd30834
ydixit-clarifai <ydixit-clarifai@users.noreply.github.com>
2026-01-13 06:40:14 +0000

    GRPC clients version e6f28be763129e6cc512646cfff6cc4fc49c8e84

--- proto/clarifai/api/resources_pb.d.ts
+++ proto/clarifai/api/resources_pb.d.ts
@@ -5,6 +5,7 @@ import * as proto_clarifai_api_status_status_code_pb from '../../../proto/clarif
 import * as proto_clarifai_api_utils_extensions_pb from '../../../proto/clarifai/api/utils/extensions_pb';
 import * as proto_clarifai_api_utils_matrix_pb from '../../../proto/clarifai/api/utils/matrix_pb';
 import * as proto_clarifai_auth_util_extension_pb from '../../../proto/clarifai/auth/util/extension_pb';
+import * as google_protobuf_duration_pb from 'google-protobuf/google/protobuf/duration_pb';
 import * as google_protobuf_struct_pb from 'google-protobuf/google/protobuf/struct_pb';
 import * as google_protobuf_timestamp_pb from 'google-protobuf/google/protobuf/timestamp_pb';
 import * as google_protobuf_wrappers_pb from 'google-protobuf/google/protobuf/wrappers_pb';
@@ -10572,6 +10573,21 @@ export class PipelineVersionRun extends jspb.Message {
   hasOrchestrationSpec(): boolean;
   clearOrchestrationSpec(): PipelineVersionRun;
 
+  getStartedAt(): google_protobuf_timestamp_pb.Timestamp | undefined;
+  setStartedAt(value?: google_protobuf_timestamp_pb.Timestamp): PipelineVersionRun;
+  hasStartedAt(): boolean;
+  clearStartedAt(): PipelineVersionRun;
+
+  getEndedAt(): google_protobuf_timestamp_pb.Timestamp | undefined;
+  setEndedAt(value?: google_protobuf_timestamp_pb.Timestamp): PipelineVersionRun;
+  hasEndedAt(): boolean;
+  clearEndedAt(): PipelineVersionRun;
+
+  getTotalRunDuration(): google_protobuf_duration_pb.Duration | undefined;
+  setTotalRunDuration(value?: google_protobuf_duration_pb.Duration): PipelineVersionRun;
+  hasTotalRunDuration(): boolean;
+  clearTotalRunDuration(): PipelineVersionRun;
+
   serializeBinary(): Uint8Array;
   toObject(includeInstance?: boolean): PipelineVersionRun.AsObject;
   static toObject(includeInstance: boolean, msg: PipelineVersionRun): PipelineVersionRun.AsObject;
@@ -10592,6 +10608,9 @@ export namespace PipelineVersionRun {
     modifiedAt?: google_protobuf_timestamp_pb.Timestamp.AsObject,
     inputArgsOverride?: OrchestrationArgsOverride.AsObject,
     orchestrationSpec?: OrchestrationSpec.AsObject,
+    startedAt?: google_protobuf_timestamp_pb.Timestamp.AsObject,
+    endedAt?: google_protobuf_timestamp_pb.Timestamp.AsObject,
+    totalRunDuration?: google_protobuf_duration_pb.Duration.AsObject,
   }
 }
 
