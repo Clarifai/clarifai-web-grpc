e699d8183c2f960bd0d2b7428265595a1e26c9ed
sainivedh <sainivedh@users.noreply.github.com>
2026-02-04 21:29:06 +0000

    GRPC clients version d57ee6011c034ea8d2956cda7d85b34309fd03b5

--- proto/clarifai/api/resources_pb.d.ts
+++ proto/clarifai/api/resources_pb.d.ts
@@ -8712,6 +8712,9 @@ export class Nodepool extends jspb.Message {
   clearSpecialHandlingList(): Nodepool;
   addSpecialHandling(value?: SpecialHandling, index?: number): SpecialHandling;
 
+  getNodeCount(): number;
+  setNodeCount(value: number): Nodepool;
+
   serializeBinary(): Uint8Array;
   toObject(includeInstance?: boolean): Nodepool.AsObject;
   static toObject(includeInstance: boolean, msg: Nodepool): Nodepool.AsObject;
@@ -8736,6 +8739,7 @@ export namespace Nodepool {
     visibility?: Visibility.AsObject,
     metadata?: google_protobuf_struct_pb.Struct.AsObject,
     specialHandlingList: Array<SpecialHandling.AsObject>,
+    nodeCount: number,
   }
 }
 
